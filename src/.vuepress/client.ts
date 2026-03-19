ebe49e5183577761a5a6b9a441db70c931242256
dter <hshm1999@foxmail.com>
2026-03-19 15:01:42 +0800

    修改主题

--- src/.vuepress/client.ts
+++
@@ -1,8 +0,0 @@
-import { defineClientConfig } from "vuepress/client";
-import FrameworkGrid from "./components/FrameworkGrid.vue";
-
-export default defineClientConfig({
-  enhance: ({ app, router, siteData }) => {
-    app.component("FrameworkGrid", FrameworkGrid);
-  },
-});
